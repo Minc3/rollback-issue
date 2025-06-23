0d1118581289f2acbda17ed88a8c726d535f1779
Minc3 <quinlan102@protonmail.com>
2025-06-23 11:30:58 +1000

    Add Redis event listeners

--- server/redis.ts
+++ server/redis.ts
@@ -8,3 +8,23 @@ export const redis = new Redis({
   password: CONFIG.REDIS_PASSWORD,
   maxRetriesPerRequest: 3,
 });
+
+redis.on('error', (error) => {
+  console.error('Redis connection error:', error);
+});
+
+redis.on('connect', () => {
+  console.log('Redis connected successfully');
+});
+
+redis.on('ready', () => {
+  console.log('Redis ready to receive commands');
+});
+
+redis.on('close', () => {
+  console.log('Redis connection closed');
+});
+
+redis.on('reconnecting', () => {
+  console.log('Redis reconnecting...');
+});
