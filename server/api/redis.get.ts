3e5d283604d88aaf1e264f589702d637dbbe0577
Minc3 <quinlan102@protonmail.com>
2025-06-23 11:26:02 +1000

    Log Redis connection errors

--- server/api/redis.get.ts
+++ server/api/redis.get.ts
@@ -5,6 +5,7 @@ export default defineEventHandler(async () => {
     await redis.get('nothing:');
     return 'redis is connected';
   } catch (error: unknown) {
+    console.error('Redis connection error:', error);
     return 'redis is not connected: ' + error?.message;
   }
 });
